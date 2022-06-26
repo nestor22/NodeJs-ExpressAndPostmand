cfef4af89e39c94c639f3c581e8d4962b14098a1
ngonzalez <nestor.ge222@gmail.com>
2022-06-25 22:20:52 -0600

    [update] service user and user schema

--- services/user.service.js
+++ services/user.service.js
@@ -8,6 +8,7 @@ class UserService {
   constructor() {}
 
   async create(data) {
+    const newUser = await models.User.create(data)
     return data;
   }
 
@@ -17,17 +18,24 @@ class UserService {
   }
 
   async findOne(id) {
-    return { id };
+    const user = await models.User.findByPk(id);
+    if(!user){
+      throw boom.notFound('user not found');
+    }
+    return user;
   }
 
   async update(id, changes) {
-    return {
-      id,
-      changes,
-    };
+
+    const user = this.findOne(id);
+    const rta = await user.update(changes);
+    return rta
   }
 
   async delete(id) {
+
+    const user = this.findOne(id);
+    await user.destroy()
     return { id };
   }
 }
