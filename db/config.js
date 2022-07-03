097bf6e037071e4793197f3d8343eea6319ee91c
ngonzalez <nestor.ge222@gmail.com>
2022-07-02 21:08:18 -0600

    [add] migrations files

---
+++ db/config.js
@@ -0,0 +1,18 @@
+const { config } = require('./../config/config');
+
+
+const USER = encodeURIComponent(config.dbUser);
+const PASSWORD = encodeURIComponent(config.dbPassword);
+const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+
+
+module.exports = {
+  devlopment: {
+    url: URI,
+    dialect: 'postgres',
+  },
+  production: {
+    url: URI,
+    dialect: 'postgres',
+  }
+}
