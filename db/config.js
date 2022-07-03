da59693ad6c8632173ecf0f47f10ddadd00551bc
ngonzalez <nestor.ge222@gmail.com>
2022-07-02 22:15:38 -0600

    [add] configuration and migrate users

--- db/config.js
+++ db/config.js
@@ -3,7 +3,7 @@ const { config } = require('./../config/config');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 
 module.exports = {
--- libs/sequelize.js
+++ libs/sequelize.js
@@ -5,15 +5,14 @@ const setupModels = require('./../db/models');
 
 const USER = encodeURIComponent(config.dbUser);
 const PASSWORD = encodeURIComponent(config.dbPassword);
-const URI = `mysql://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
+const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`;
 
 const sequelize = new Sequelize(URI, {
-  dialect: 'mysql',
+  dialect: 'postgres',
   logging: true,
 });
 
 setupModels(sequelize);
 
-sequelize.sync();
 
 module.exports = sequelize;
