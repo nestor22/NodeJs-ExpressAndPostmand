1ff2bdc1702532c205554f0758ec918d08a4797f
ngonzalez <nestor.ge222@gmail.com>
2022-07-06 22:53:01 -0600

    [add] relations on the categoryses and products

--- db/models/index.js
+++ db/models/index.js
@@ -1,14 +1,19 @@
 const { User, UserSchema } = require('./user.model');
 const { Customer, CustomerSchema } = require('./customer.model');
+const { Category, CategorySchema } = require('./categories.model');
+const { Product, ProductSchema } = require('./products.model');
+
 
 
 function setupModels(sequelize){
   User.init(UserSchema, User.config(sequelize));
   Customer.init(CustomerSchema, Customer.config(sequelize));
+  Category.init(CategorySchema, Category.config(sequelize));
+  Product.init(ProductSchema, Product.config(sequelize));
 
   User.associate(sequelize.models)
-
   Customer.associate(sequelize.models)
+  Category.associate(sequelize.models)
 }
 
 module.exports = setupModels;
--- db/models/products.model.js
+++ db/models/products.model.js
@@ -1,10 +1,11 @@
 const { Model, DataTypes, Sequelize } = require('sequelize');
+const { CATEGORY_TABLE } = require('./categories.model')
 
 const PRODUCTS_TABLE = 'products';
 
 const ProductSchema = {
   id: {
-    allowNull: false,
+    allowNull: true,
     autoImcrement: true,
     primaryKey: true,
     type: DataTypes.INTEGER
@@ -12,28 +13,45 @@ const ProductSchema = {
   name: {
     allowNull: false,
     type: DataTypes.STRING,
-    unique: true,
+  },
+  image: {
+    allowNull: false,
+    type: DataTypes.STRING
+  },
+  description: {
+    allowNull: false,
+    type: DataTypes.TEXT
   },
   price: {
     allowNull: false,
-    type: DataTypes.DECIMAL
-  },
-  image: {
-    allowNull: true,
-    type: DataTypes.STRING
+    type: DataTypes.INTEGER
   },
   createdAt:{
     allowNull: false,
     type: DataTypes.DATE,
     field: 'create_at',
     defaultValue: Sequelize.NOW
+  },
+  categoryId: {
+    field: 'category_id',
+    allowNull: false,
+    type: DataTypes.INTEGER,
+    references:{
+      model: CATEGORY_TABLE,
+      key: 'id'
+    },
+    onUpdate: 'CASCADE',
+    onDelete: 'SET NULL'
+
   }
 
 }
 
-class Products extends Model{
-  static associate(){
-    //models
+class Product extends Model{
+  static associate(models){
+    this.belongsTo(models.Category, {
+      as: 'category'
+    })
   }
 
   static config(sequelize){
@@ -46,4 +64,4 @@ class Products extends Model{
   }
 }
 
-module.exports = {PRODUCTS_TABLE, ProductSchema, Products}
+module.exports = {PRODUCTS_TABLE, ProductSchema, Product}
